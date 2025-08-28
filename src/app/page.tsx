d719617a6c78feffeb8c415643e680ca3479556c
ECX Developers <dev.encryptarx@gmail.com>
2025-08-28 13:39:40 +0000

    in the main home page (1st landing page) remove the how it works section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,6 @@ import { CtaSection } from '@/components/sections/cta-section';
 import { Footer } from '@/components/layout/footer';
 import { ParallaxProvider } from '@/components/providers/parallax-provider';
 import { DeployableWorkSection } from '@/components/sections/deployable-work-section';
-import { HowItWorksSection } from '@/components/sections/how-it-works-section';
-import { FeaturesSection } from '@/components/sections/features-section';
 
 export default function Home() {
   return (
@@ -15,8 +13,6 @@ export default function Home() {
         <ParallaxProvider>
           <HeroSection />
           <DeployableWorkSection />
-          <HowItWorksSection />
-          <FeaturesSection />
           <CtaSection />
         </ParallaxProvider>
       </main>
